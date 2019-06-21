bd3ad3bf4714b705842cb22e8b8a1aa3bb463c75
rjrj98 <rene.j98@hotmail.com>
2019-06-22 01:18:36 +0200

    Commenting and Building basic structur for simd. Still did not find operator for vertical add

--- BMP.s
+++ BMP.s
@@ -1,18 +1,23 @@
 .intel_syntax noprefix
 .global greyscale 
 
+.data
+
+greyWeighting:      .byte 0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01
+
+
 .text
 
 
 # void greyscale(RGB* out, int width, int height);
 
-#-------------------Bei der Übergabe----------------------------
-#
-#	# rdi = Adress for color file data and for writing data back
-# 	# rsi = width
-#	# rcx = height
-#
-#---------------------------------------------------------------
+#-------------------Bei der Übergabe--------------------#
+#							#
+#	# rdi = Adress pixel Array (Read and write)	#
+# 	# rsi = width					#	
+#	# rcx = height					#
+#							#	
+#-------------------------------------------------------#
 
 greyscale:
 
@@ -42,22 +47,22 @@ greyscale:
 
 
 		
-	xor rax, rax
-	xor r8, r8	
+	xor rax, rax		#For copying from Memory
+	xor r8, r8		#For storing average	
 
 	mov al, [rdi + r10] 	#Blue channel
 	lea r8d, [eax * 1]	#Add to avg with weighting
 
 	mov al, [rdi+ r10 + 1] 	#Green channel
-	lea r8d, [r8d + eax * 1]#Add to avg with weighting
+	lea r8d,[r8d + eax * 1]	#Add to avg with weighting
 	
 	mov al, [rdi+ r10 + 2]	#Red channel
-	lea r8d, [r8d + eax * 1]#Add to avg with weighting
+	lea r8d,[r8d + eax * 1]	#Add to avg with weighting
 
 	mov eax, r8d
 	mov r8, 3
 
-	xor rdx, rdx
+	xor rdx, rdx		#For devision
 	
 	div r8
 
@@ -70,19 +75,22 @@ greyscale:
 	add r10, 3 #Move to the next pixel
 	jmp .Lloop
 
-
-
-		
-
-
-    
-
-
-
-
 .Lend:
-
 ret
+
+greyscale_simd:
+
+jmp greyscale
+
+
+
+
+
+
+
+
+
+
 
 
 // void blur(File* in, File* out, int width, int height)
