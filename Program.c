0276fb984696ba1d0cac7bdcb967524c54017077
Philipp1999 <philipp.zagar@gmail.com>
2019-06-23 19:41:23 +0200

    Add some raw code and JPEG library for JPEG conversion

--- Program.c
+++ Program.c
@@ -2,7 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "./jpeg-6b/jpeglib.h>
 #include "BMPStructs.h"
+
+
+/* we will be using this uninitialized pointer later to store raw, uncompressd image */
+unsigned char *raw_image = NULL;
+
+/* dimensions of the image we want to write */
+int width;
+int height;
+int bytes_per_pixel;   /* or 1 for GRACYSCALE images */
+int color_space; /* or JCS_GRAYSCALE for grayscale images */
+
 
 // Assembly Functions
 extern void greyscale(RGB *rgbValuesOut, int width, int height);
@@ -45,7 +57,7 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    // Reading header and check validy (Magic Number) of BMP File
+    // Reading header of BMP File
     BMPHeader* header = readHeader(inFile);
     if(!header) {
         printf("Error while reading header\n");
@@ -421,4 +433,84 @@ void convolutionRGB(RGB* rgbValues, BMPImageInfo* info) {
             sumRed = 0; sumGreen = 0; sumBlue = 0;
         }
     }
+}
+
+/**
+ * read_jpeg_file Reads from a jpeg file on disk specified by filename and saves into the
+ * raw_image buffer in an uncompressed format.
+ *
+ * \returns positive integer if successful, -1 otherwise
+ * \param *filename char string specifying the file name to read from
+ *
+ */
+
+int read_jpeg_file( char *filename )
+{
+    /* these are standard libjpeg structures for reading(decompression) */
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    /* libjpeg data structure for storing one row, that is, scanline of an image */
+    JSAMPROW row_pointer[1];
+
+    FILE *infile = fopen( filename, "rb" );
+    unsigned long location = 0;
+    int i = 0;
+
+    if ( !infile )
+    {
+        printf("Error opening jpeg file %s\n!", filename );
+        return -1;
+    }
+    /* here we set up the standard libjpeg error handler */
+    cinfo.err = jpeg_std_error( &jerr );
+    /* setup decompression process and source, then read JPEG header */
+    jpeg_create_decompress( &cinfo );
+    /* this makes the library read from infile */
+    jpeg_stdio_src( &cinfo, infile );
+    /* reading the image header which contains image information */
+    jpeg_read_header( &cinfo, TRUE );
+    /* Uncomment the following to output image information, if needed. */
+
+    printf( "JPEG File Information: \n" );
+    printf( "Image width and height: %d pixels and %d pixels.\n", width=cinfo.image_width, height=cinfo.image_height );
+    printf( "Color components per pixel: %d.\n", bytes_per_pixel = cinfo.num_components );
+    printf( "Color space: %d.\n", cinfo.jpeg_color_space );
+
+    /* Start decompression jpeg here */
+    jpeg_start_decompress( &cinfo );
+
+    /* allocate memory to hold the uncompressed image */
+    raw_image = (unsigned char*)malloc( cinfo.output_width*cinfo.output_height*cinfo.num_components );
+    /* now actually read the jpeg into the raw buffer */
+    row_pointer[0] = (unsigned char *)malloc( cinfo.output_width*cinfo.num_components );
+    /* read one scan line at a time */
+    while( cinfo.output_scanline < cinfo.image_height )
+    {
+        jpeg_read_scanlines( &cinfo, row_pointer, 1 );
+        for( i=0; i<cinfo.image_width*cinfo.num_components;i++)
+            raw_image[location++] = row_pointer[0][i];
+    }
+    /* wrap up decompression, destroy objects, free pointers and close open files */
+    jpeg_finish_decompress( &cinfo );
+    jpeg_destroy_decompress( &cinfo );
+    free( row_pointer[0] );
+    fclose( infile );
+    /* yup, we succeeded! */
+    return 1;
+}
+
+
+int main(int argc,char **argv)
+{
+    int x,y;
+
+    if(argc != 3){ printf("Usage: %s source.jpg dest.bmp",argv[0]); return -1; }
+    x=y=0;
+    /* Try opening a jpeg*/
+    if( read_jpeg_file( argv[1] ) > 0 )   write_bmp_file( argv[2] );
+
+    else return -1;
+
+    free(raw_image);
+    return 0;
 }
