3515eced11afd9b86d20cb53b4c00fa2b013b1d8
ge25hut <florian.maximilian.huber@tum.de>
2019-07-02 14:28:20 +0200

    blur vorerst für mittlere Elemente

--- BMP.s
+++ BMP.s
@@ -156,7 +156,7 @@ greyscale_simd:
 		
 
 
-	
+ret	
 
 
 
@@ -164,9 +164,309 @@ greyscale_simd:
 
 
 
-
-// void blur(File* in, File* out, int width, int height)
 .global blur
-blur:
-    ret
 
+# void blur(File* in, File* out, int width, int height)
+
+    # rdi = Adress for greyscale data
+ 	# rsi = Adress for writing data back
+	# rdx = width  --> r15 (otherwise problem with division)
+	# rcx = height
+
+blur:
+	push r15
+
+	mov r15, rdx #moved width for division                           drandenken dass alles verschoben sein kann wegen anderen parametern
+	
+	xor rdx, rdx
+	
+	push rbx
+	
+	#register für Ränder###################	
+	push r12 #oberste Zeile
+	mov r12, rcx
+	dec r12
+	
+	push r13 #linkeste Spalte
+	mov r13, r15
+	dec r13	
+	#######################################
+	
+	push r14 #speicherort für blurberechnung.......
+
+
+	xor r10, r10 #Height counter = 0
+
+
+.LloopHeight:
+
+	cmp r10, rcx
+	jge .Lendd	#if(r10 >= height)
+	xor r11, r11 	#Width counter = 0
+
+
+.Lloopwidth:
+	cmp r11, r15
+	jge .LincCounterHeight #if(r11 >= width)
+
+	mov rbx, r10 # which level we are on
+	imul rbx, r15 # Multiply by the pixels of before
+	add rbx, r11 # in which pixel we are
+
+	#Überprüfung auf Stelle in Matrix und vllt Weiterschicken
+	cmp r10, 0
+	je .LuntererRand
+	
+	cmp r10, r12
+	je .LobererRand
+	
+	cmp r11, 0
+	je .LlinkerRand
+	
+	cmp r11, r13
+	je .LrechterRand
+	
+	#Operationen für mittlere Elemente
+	xor rax, rax
+	xor r14, r14
+	
+	#mittleres Element
+	imul rbx, 3 #nicht nur pixel sondern auch 3 farbkanäle berücksichtigen
+	
+	mov rax, 4
+	imul rax, [rdi + rbx]
+	add r14, rax
+	
+	#linkes Element
+	xor rax, rax #nötig?
+	
+	mov rbx, r10
+	imul rbx, r15
+	add rbx, r11
+	
+	dec rbx #wegen linkes element
+	
+	imul rbx, 3
+	
+	mov rax, 2
+	imul rax, [rdi + rbx]
+	add r14, rax
+	
+	#links oben Element
+	xor rax, rax
+	
+	inc r10
+	mov rbx, r10
+	dec r10
+	imul rbx, r15
+	add rbx, r11
+	
+	dec rbx
+	
+	imul rbx, 3
+	
+	mov rax, 1
+	imul rax, [rdi + rbx]
+	add r14, rax
+	
+	
+	
+	#oben Element
+	xor rax, rax
+	
+	inc r10
+	mov rbx, r10
+	dec r10
+	imul rbx, r15
+	add rbx, r11
+	
+	imul rbx, 3
+	
+	mov rax, 2
+	imul rax, [rdi + rbx]
+	add r14, rax
+	
+	#rechts oben Element
+	xor rax, rax
+	
+	inc r10
+	mov rbx, r10
+	dec r10
+	imul rbx, r15
+	add rbx, r11
+	
+	inc rbx
+	
+	imul rbx, 3
+	
+	mov rax, 1
+	imul rax, [rdi + rbx]
+	add r14, rax
+	
+	#rechts Element
+	xor rax, rax
+	
+	mov rbx, r10
+	imul rbx, r15
+	add rbx, r11
+	
+	inc rbx
+	
+	imul rbx, 3
+	
+	mov rax, 2
+	imul rax, [rdi + rbx]
+	add r14, rax
+	
+	#rechts unten Element
+	xor rax, rax
+	
+	dec r10
+	mov rbx, r10
+	inc r10
+	imul rbx, r15
+	add rbx, r11
+	
+	inc rbx
+	
+	imul rbx, 3
+	
+	mov rax, 1
+	imul rax, [rdi + rbx]
+	add r14, rax
+	
+	#unten Element
+	xor rax, rax
+	
+	dec r10
+	mov rbx, r10
+	inc r10
+	imul rbx, r15
+	add rbx, r11
+	
+	dec rbx
+	
+	imul rbx, 3
+	
+	mov rax, 2
+	imul rax, [rdi + rbx]
+	add r14, rax
+	
+	#links unten Element
+	xor rax, rax
+	
+	dec r10
+	mov rbx, r10
+	inc r10
+	imul rbx, r15
+	add rbx, r11
+	
+	dec rbx
+	
+	imul rbx, 3
+	
+	mov rax, 1
+	imul rax, [rdi + rbx]
+	add r14, rax
+	
+	#r10 und r11 müssen nicht auf Mitte gesetzt werden weil unverändert
+	
+	xor rax, rax
+	
+	mov rax, r14
+	mov r14, 16
+	idiv r14 #alles durch 16 teilen
+	
+	#auf neuem pointer speichern...
+	#auf alle drei Farbkanäle schreiben und nicht nur auf einen!!
+	mov [rsi + rbx], r14
+	mov [rsi + rbx + 1], r14
+	mov [rsi + rbx + 2], r14	
+	
+	
+.Lrücksprung: #nötig falls Position nicht Mitte ist
+  	
+	inc r11
+	jmp .Lloopwidth
+
+
+.LincCounterHeight:
+	inc r10
+	jmp .LloopHeight
+
+#Ränder                                      (erstmal übersprungen, kann probleme im output erzeugen weil gar nicht drin , hoffenltihc als 0 drin)
+.LuntererRand:
+#Überprüfung ob Ecke
+cmp r11, 0
+je .LlinkeUnterEcke
+	
+cmp r11, r13
+je .LrechteUnterEcke
+
+
+
+jmp .Lrücksprung
+
+
+.LobererRand:
+#Überprüfung ob Ecke
+cmp r11, 0
+je .LlinkeObereEcke
+	
+cmp r11, r13
+je .LrechteObereEcke
+
+
+
+jmp .Lrücksprung
+
+.LlinkerRand:
+#Überprüfung auf Ecke unnötig sonst schon in einen der Ränder gelandet
+
+
+
+jmp .Lrücksprung
+
+.LrechterRand:
+#Überprüfung auf Ecke unnötig sonst schon in einen der Ränder gelandet
+
+
+
+jmp .Lrücksprung	
+
+#Ecken
+.LlinkeUnterEcke:
+
+
+
+jmp .Lrücksprung
+
+.LrechteUnterEcke:
+
+
+
+jmp .Lrücksprung
+
+.LlinkeObereEcke:
+
+
+
+jmp .Lrücksprung
+
+.LrechteObereEcke:
+
+
+
+jmp .Lrücksprung
+
+
+
+
+.Lendd:
+
+pop r14
+pop r13
+pop r12
+pop rbx
+pop r15
+ret
