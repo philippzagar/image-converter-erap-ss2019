dd5402e3db7e4365ef6ec5bd5b421f2bc668d26b
RJ <you@example.com>
2019-07-02 23:33:20 +0200

    Fixed Wrong adressing

--- BMP.s
+++ BMP.s
@@ -1,5 +1,5 @@
 .intel_syntax noprefix
-.global greyscale 
+.global greyscale
 .global greyscale_simd
 
 .data
@@ -15,10 +15,10 @@ greyWeighting:      .byte 0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01
 #-------------------Bei der Übergabe--------------------#
 #							#
 #	# rdi = Adress pixel Array (Read and write)	#
-# 	# rsi = width					#	
-#	# rdx = height					#	
+# 	# rsi = width					#
+#	# rdx = height					#
 #							#
-#	--> Keine Rückgabe erwartet			#	
+#	--> Keine Rückgabe erwartet			#
 #-------------------------------------------------------#
 
 greyscale:
@@ -26,17 +26,17 @@ greyscale:
 	#r10 = counter for loop
 	#rsi = amountOfChannels
 	#r8 = calculating average
-	#height moved to rcx for devision 
-	
+	#height moved to rcx for devision
 
-	
+
+
 
 	mov rcx, rdx 		#moved height for devision
-	
+
 	imul rsi, rcx 		#pixelAmount = height * width
 	imul rsi, 3 		#ChannelAmount = pixelAmount * 3
 
-	
+
 
 
 	xor r10, r10 		#loop Counter = 0Retry
@@ -49,16 +49,16 @@ greyscale:
 
 
 
-		
+
 	xor rax, rax		#For copying from Memory
-	xor r8, r8		#For storing average	
+	xor r8, r8		#For storing average
 
 	mov al, [rdi + r10] 	#Blue channel
 	lea r8d, [eax * 1]	#Add to avg with weighting
 
 	mov al, [rdi+ r10 + 1] 	#Green channel
 	lea r8d,[r8d + eax * 1]	#Add to avg with weighting
-	
+
 	mov al, [rdi+ r10 + 2]	#Red channel
 	lea r8d,[r8d + eax * 1]	#Add to avg with weighting
 
@@ -66,22 +66,22 @@ greyscale:
 	mov r8, 3
 
 	xor rdx, rdx		#For devision
-	
+
 	div r8
 
 	mov [rdi + r10], al  	#Blue channel
 	mov [rdi+ r10 + 1], al	#Green channel
 	mov [rdi+ r10 + 2], al	#Red channel
 
-	
-  	
+
+
 	add r10, 3 #Move to the next pixel
 	jmp .Lloop
 
 .Lend:
 	ret
 
-#**************************************************************
+#********************************************************
 
 
 # void greyscale_simd(RGB* out, int width, int height);
@@ -89,10 +89,10 @@ greyscale:
 #-------------------Bei der Übergabe--------------------#
 #							#
 #	# rdi = Adress pixel Array (Read and write)	#
-# 	# rsi = width					#	
-#	# rdx = height					#	
+# 	# rsi = width					#
+#	# rdx = height					#
 #						#
-#	--> Keine Rückgabe erwartet			#	
+#	--> Keine Rückgabe erwartet			#
 #-------------------------------------------------------#
 
 
@@ -119,7 +119,7 @@ greyscale_simd:
 
 
 
-	
+
 
 
 .Lload:
@@ -141,7 +141,7 @@ greyscale_simd:
 	jmp .Lload
 
 	#todo:
-	#load only pixel row that fully fits into simd 
+	#load only pixel row that fully fits into simd
 	#do the rest with non
 
 
@@ -153,58 +153,59 @@ greyscale_simd:
 	#add rdi how many pixels where loaded and processed
 
 	jmp greyscale # Jump to grmultiplyey for pixels that do not fit
-		
-
-
-ret	
 
 
 
+ret
 
+
+
+#******************************************************************************
+#************************************BLUR**************************************
+#******************************************************************************
 
 
 
 .global blur
 
-# void blur(File* in, File* out, int width, int height)
+# void blur(RGB* in, RGB* out, int width, int height)
 
-    # rdi = Adress for greyscale data
+  # rdi = Adress for greyscale data
  	# rsi = Adress for writing data back
 	# rdx = width  --> r15 (otherwise problem with division)
 	# rcx = height
 
 blur:
-	push r15 #width
-	push rbx #iterator
+	push r15 			#width
+	push rbx 			#iterator
 
-	mov r15, rdx #moved width for division	
+	mov r15, rdx 	#moved width for division
 	xor rdx, rdx
-		
-	
+
+
 	#Register, die als Markierung der Ränder benutzt werden#
 	#oberste Zeile
 	push r12
-	mov r12, rcx
+	mov r12, rcx 	#Height
 	dec r12
-	
+
 	#rechteste Spalte
 	push r13
-	mov r13, r15
+	mov r13, r15	#Width
 	dec r13
-	
+
 	#linkeste Spalte und unterste Zeile immer 1 (kein Register nötig)
-	##########################################################
-	
-	push r14 #speicherort für blurberechnung
+
+	push r14 			#speicherort für blurberechnung
 
 
-	xor r10, r10 #Height counter = 0
+	xor r10, r10 	#Height counter = 0
 
 
 .LloopHeight:
 
 	cmp r10, rcx
-	jge .Lendd	#if(r10 >= height)
+	jge .Lendd		#if(r10 >= height)
 	xor r11, r11 	#Width counter = 0
 
 
@@ -212,207 +213,199 @@ blur:
 	cmp r11, r15
 	jge .LincCounterHeight #if(r11 >= width)
 
-	#Überprüfung auf Stelle in Matrix und vllt Weiterschicken##################
+	#*****************Special cases for border pixels****************************
 	cmp r10, 0
 	je .LuntererRand
-	
+
 	cmp r10, r12
 	je .LobererRand
-	
+
 	cmp r11, 0
 	je .LlinkerRand
-	
+
 	cmp r11, r13
 	je .LrechterRand
-	###########################################################################
-	
-	#Operationen für mittlere Elemente#############################################################
+
+
+	#*************************Normal Pixels**************************************
 	xor r14, r14
-	
-	#mittleres Element#########################################################
+
+	#mittleres Element##################
 	xor rax, rax
 	xor rdx, rdx
-	xor rbx, rbx #nötig??
-	
-	mov rbx, r10 # which level we are on
-	imul rbx, r15 # Multiply by the pixels of before
-	add rbx, r11 # in which pixel we are
-	
-	imul rbx, 3 #nicht nur pixel sondern auch 3 farbkanäle berücksichtigen
-	
-	mov rax, 4
-	imul rax, [rdi + rbx]
+
+
+	mov rbx, r10 	# which level we are on
+	imul rbx, r15 # Multiply by Width --> all pixels before
+	add rbx, r11 	# in which pixel we are
+
+	imul rbx, 3 	# 3 Byte per pixel
+
+	mov al, [rdi + rbx]
+	imul rax, 4
 	add r14, rax
-	
-	#linkes Element################################################################
-	xor rax, rax #nötig?
+
+	#linkes Element#####################
+	xor rax, rax
 	xor rdx, rdx
-	xor rbx, rbx #nötig??
-	
+
 	mov rbx, r10
 	imul rbx, r15
 	add rbx, r11
-	
+
 	dec rbx #wegen linkes element
-	
+
 	imul rbx, 3
-	
-	mov rax, 2
-	imul rax, [rdi + rbx]
+
+	mov al, [rdi + rbx]
+	imul rax, 2
 	add r14, rax
-	
+
 	#links oben Element###############################################################
 	xor rax, rax
 	xor rdx, rdx
-	xor rbx, rbx #nötig??
-	
+
 	inc r10 #wegen oben Element
 	mov rbx, r10
 	dec r10 #wegen oben Element rückgängig
 	imul rbx, r15
 	add rbx, r11
-	
+
 	dec rbx #wegen linkes element
-	
+
 	imul rbx, 3
-	
-	mov rax, 1
-	imul rax, [rdi + rbx]
+
+	mov al, [rdi + rbx]
+	imul rax, 1
 	add r14, rax
-	
+
 	#oben Element########################################################################
 	xor rax, rax
 	xor rdx, rdx
-	xor rbx, rbx #nötig??
-	
+
 	inc r10 #wegen oben Element
 	mov rbx, r10
 	dec r10 #wegen oben Element rückgängig
 	imul rbx, r15
 	add rbx, r11
-	
+
 	imul rbx, 3
-	
-	mov rax, 2
-	imul rax, [rdi + rbx]
+
+	mov al, [rdi + rbx]
+	imul rax, 2
 	add r14, rax
-	
+
 	#rechts oben Element#####################################################################
 	xor rax, rax
 	xor rdx, rdx
-	xor rbx, rbx #nötig??
-	
+
+
 	inc r10 #wegen oben Element
 	mov rbx, r10
 	dec r10 #wegen oben Element rückgängig
 	imul rbx, r15
 	add rbx, r11
-	
+
 	inc rbx #wegen rechts
-	
+
 	imul rbx, 3
-	
-	mov rax, 1
-	imul rax, [rdi + rbx]
+
+	mov al, [rdi + rbx]
+	imul rax, 1
 	add r14, rax
-	
+
 	#rechts Element############################################################################
 	xor rax, rax
 	xor rdx, rdx
-	xor rbx, rbx #nötig??
-	
+
 	mov rbx, r10
 	imul rbx, r15
 	add rbx, r11
-	
+
 	inc rbx #wegen rechts
-	
+
 	imul rbx, 3
-	
-	mov rax, 2
-	imul rax, [rdi + rbx]
+
+	mov al, [rdi + rbx]
+	imul rax, 2
 	add r14, rax
-	
+
 	#rechts unten Element######################################################################
 	xor rax, rax
 	xor rdx, rdx
-	xor rbx, rbx #nötig??
-	
+
 	dec r10 #wegen unten
 	mov rbx, r10
 	inc r10 #wegen unten Element rückgängig
 	imul rbx, r15
 	add rbx, r11
-	
+
 	inc rbx #wegen rechts
-	
+
 	imul rbx, 3
-	
-	mov rax, 1
-	imul rax, [rdi + rbx]
+
+	mov al, [rdi + rbx]
+	imul rax, 1
 	add r14, rax
-	
+
 	#unten Element##############################################################################
 	xor rax, rax
 	xor rdx, rdx
-	xor rbx, rbx #nötig??
-	
+
 	dec r10 #wegen unten
 	mov rbx, r10
 	inc r10 #wegen unten Element rückgängig
 	imul rbx, r15
 	add rbx, r11
-	
+
 	imul rbx, 3
-	
-	mov rax, 2
-	imul rax, [rdi + rbx]
+
+	mov al, [rdi + rbx]
+	imul rax, 2
 	add r14, rax
-	
+
 	#links unten Element##########################################################################
 	xor rax, rax
 	xor rdx, rdx
-	xor rbx, rbx #nötig??
-	
+
 	dec r10 #wegen unten
 	mov rbx, r10
 	inc r10 #wegen unten Element rückgängig
 	imul rbx, r15
 	add rbx, r11
-	
+
 	dec rbx #wegen links
-	
+
 	imul rbx, 3
-	
-	mov rax, 1
+
+	mov al, [rdi + rbx]
 	imul rax, [rdi + rbx]
 	add r14, rax
-	
+
 	#r10 und r11 müssen nicht auf Mitte gesetzt werden weil unverändert
-	
+
 	xor rax, rax
 	xor rdx, rdx
 	xor rbx, rbx
-	
+
 	#nochmal mittleres Element fürs zurückschreiben bestimmen!!!
-	mov rbx, r10 # which level we are on
-	imul rbx, r15 # Multiply by the pixels of before
-	add rbx, r11 # in which pixel we are	
-	imul rbx, 3 #nicht nur pixel sondern auch 3 farbkanäle berücksichtigen
-	
+	mov rbx, r10 		# which level we are on[rdi + rbx]
+	imul rbx, r15 	# Multiply by the pixels of before
+	add rbx, r11 		# in which pixel we are
+	imul rbx, 3		  # 3 Byte per pixel
+
 	mov rax, r14
 	mov r14, 16
-	idiv r14 #alles durch 16 teilen
-	
+	idiv r14 				#alles durch 16 teilen
+
 	#auf neuem pointer speichern...
 	#auf alle drei Farbkanäle schreiben und nicht nur auf einen!!
-	mov [rsi + rbx], rax
-	mov [rsi + rbx + 1], rax
-	mov [rsi + rbx + 2], rax
-	
-	
+	mov [rsi + rbx], al			#Blue
+	mov [rsi + rbx + 1], al	#Green
+	mov [rsi + rbx + 2], al	#Red
+
+
 .Lrücksprung: #nötig falls Position nicht Mitte ist
-  	
 	inc r11
 	jmp .Lloopwidth
 
@@ -421,72 +414,62 @@ blur:
 	inc r10
 	jmp .LloopHeight
 
+#*************************Special Case implementation***************************
+
+
 #Ränder                                      (erstmal übersprungen, kann probleme im output erzeugen weil gar nicht drin , hoffenltihc als 0 drin)
 .LuntererRand:
 #Überprüfung ob Ecke
-cmp r11, 0
-je .LlinkeUnterEcke
-	
-cmp r11, r13
-je .LrechteUnterEcke
+	cmp r11, 0
+	je .LlinkeUnterEcke
+
+	cmp r11, r13
+	je .LrechteUnterEcke
 
 
 
-jmp .Lrücksprung
+	jmp .Lrücksprung
 
 
 .LobererRand:
-#Überprüfung ob Ecke
-cmp r11, 0
-je .LlinkeObereEcke
-	
-cmp r11, r13
-je .LrechteObereEcke
+	#Überprüfung ob Ecke
+	cmp r11, 0
+	je .LlinkeObereEcke
+
+	cmp r11, r13
+	je .LrechteObereEcke
 
 
 
-jmp .Lrücksprung
+	jmp .Lrücksprung
 
 .LlinkerRand:
-#Überprüfung auf Ecke unnötig sonst schon in einen der Ränder gelandet
-
-
-
-jmp .Lrücksprung
+	#Überprüfung auf Ecke unnötig sonst schon in einen der Ränder gelandet
+	jmp .Lrücksprung
 
 .LrechterRand:
-#Überprüfung auf Ecke unnötig sonst schon in einen der Ränder gelandet
-
-
-
-jmp .Lrücksprung	
+	#Überprüfung auf Ecke unnötig sonst schon in einen der Ränder gelandet
+	jmp .Lrücksprung
 
 #Ecken
 .LlinkeUnterEcke:
 
+	jmp .Lrücksprung
 
-
-jmp .Lrücksprung
 
 .LrechteUnterEcke:
 
+	jmp .Lrücksprung
 
-
-jmp .Lrücksprung
 
 .LlinkeObereEcke:
 
+	jmp .Lrücksprung
 
-
-jmp .Lrücksprung
 
 .LrechteObereEcke:
 
-
-
-jmp .Lrücksprung
-
-
+	jmp .Lrücksprung
 
 
 .Lendd:
